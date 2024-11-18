c007fffd4895d66d1d83690a6f2c61f1ecc52e51
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-18 17:47:13 -0400

    fix(admin): register page url fix

--- apps/admin/src/app/auth/register/page.tsx
+++ apps/admin/src/app/auth/register/page.tsx
@@ -1,5 +1,18 @@
 import { AuthPage } from "@refinedev/antd";
+import { Link } from "@refinedev/core";
 
 export default function RegisterPage() {
-  return <AuthPage type="register" />;
+  return (
+    <AuthPage
+      type="register"
+      loginLink={
+        <span className="text-xs flex flex-1 justify-end">
+          Have an account?&nbsp;
+          <Link to="/auth/login">
+            <span className="text-blue-400 font-medium">Sign In</span>
+          </Link>
+        </span>
+      }
+    />
+  );
 }

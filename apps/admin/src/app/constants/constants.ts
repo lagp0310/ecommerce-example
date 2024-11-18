5069aaf5c53c317a879c970439411e8f9ba373c4
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-18 12:34:58 -0400

    chore(admin): reorder resources by name

--- apps/admin/src/app/constants/constants.ts
+++ apps/admin/src/app/constants/constants.ts
@@ -1,31 +1,12 @@
 export const refineResources = [
   {
-    name: "customers",
-    list: "/customers",
-    create: "/customers/create",
-    edit: "/customers/edit/:id",
-    meta: { canDelete: true },
-  },
-  {
-    name: "users",
-    list: "/users",
-    create: "/users/create",
-    edit: "/users/edit/:id",
-    meta: { canDelete: true },
-  },
-  {
-    name: "products",
-    list: "/products",
-    create: "/products/create",
-    edit: "/products/edit/:id",
-    meta: { canDelete: true },
-  },
-  {
     name: "banner_types",
     list: "/banner-types",
     create: "/banner-types/create",
     edit: "/banner-types/edit/:id",
-    meta: { canDelete: true },
+    meta: {
+      canDelete: true,
+    },
   },
   {
     name: "businesses",
@@ -38,14 +19,18 @@ export const refineResources = [
     list: "/categories",
     create: "/categories/create",
     edit: "/categories/edit/:id",
-    meta: { canDelete: true },
+    meta: {
+      canDelete: true,
+    },
   },
   {
     name: "coupon_codes",
     list: "/coupon-codes",
     create: "/coupon-codes/create",
     edit: "/coupon-codes/edit/:id",
-    meta: { canDelete: true },
+    meta: {
+      canDelete: true,
+    },
   },
   {
     name: "currencies",
@@ -54,44 +39,83 @@ export const refineResources = [
     edit: "/currencies/edit/:id",
   },
   {
-    name: "locales",
-    list: "/locales",
-    meta: { canDelete: false },
-  },
-  {
-    name: "store_features",
-    list: "/store-features",
-    create: "/store-features/create",
-    edit: "/store-features/edit/:id",
-    meta: { canDelete: true },
-  },
-  {
-    name: "stores",
-    list: "/stores",
-    create: "/stores/create",
-    edit: "/stores/edit/:id",
-    meta: { canDelete: true },
+    name: "customers",
+    list: "/customers",
+    create: "/customers/create",
+    edit: "/customers/edit/:id",
+    meta: {
+      canDelete: true,
+    },
   },
   {
     name: "header_banners",
     list: "/store-header-banners",
     create: "/store-header-banners/create",
     edit: "/store-header-banners/edit/:id",
-    meta: { canDelete: true },
-  },
-  {
-    name: "offer_banners",
-    list: "/store-offer-banners",
-    create: "/store-offer-banners/create",
-    edit: "/store-offer-banners/edit/:id",
-    meta: { canDelete: true },
+    meta: {
+      canDelete: true,
+    },
   },
   {
     name: "image_banners",
     list: "/store-image-banners",
     create: "/store-image-banners/create",
     edit: "/store-image-banners/edit/:id",
-    meta: { canDelete: true },
+    meta: {
+      canDelete: true,
+    },
+  },
+  {
+    name: "locales",
+    list: "/locales",
+    meta: {
+      canDelete: false,
+    },
+  },
+  {
+    name: "offer_banners",
+    list: "/store-offer-banners",
+    create: "/store-offer-banners/create",
+    edit: "/store-offer-banners/edit/:id",
+    meta: {
+      canDelete: true,
+    },
+  },
+  {
+    name: "products",
+    list: "/products",
+    create: "/products/create",
+    edit: "/products/edit/:id",
+    meta: {
+      canDelete: true,
+    },
+  },
+  {
+    name: "stores",
+    list: "/stores",
+    create: "/stores/create",
+    edit: "/stores/edit/:id",
+    meta: {
+      canDelete: true,
+    },
+  },
+  {
+    name: "store_features",
+    list: "/store-features",
+    create: "/store-features/create",
+    edit: "/store-features/edit/:id",
+    meta: {
+      canDelete: true,
+    },
+  },
+  {
+    name: "users",
+    list: "/users",
+    create: "/users/create",
+    edit: "/users/edit/:id",
+    meta: {
+      canDelete: true,
+    },
   },
 ];
 

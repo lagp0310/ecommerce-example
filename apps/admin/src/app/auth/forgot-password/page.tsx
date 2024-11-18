2afce54536f0e8947fa5d8aff9586f13607704b8
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-18 17:42:47 -0400

    fix(admin): auth pages routes update

--- apps/admin/src/app/auth/forgot-password/page.tsx
+++ apps/admin/src/app/auth/forgot-password/page.tsx
@@ -1,5 +1,18 @@
 import { AuthPage } from "@refinedev/antd";
+import { Link } from "@refinedev/core";
 
 export default function ForgotPasswordPage() {
-  return <AuthPage type="forgotPassword" />;
+  return (
+    <AuthPage
+      type="forgotPassword"
+      loginLink={
+        <span className="flex flex-1 justify-end text-xs">
+          Have an account?&nbsp;
+          <Link to="/auth/login">
+            <span className="text-blue-400 font-medium">Sign In</span>
+          </Link>
+        </span>
+      }
+    />
+  );
 }

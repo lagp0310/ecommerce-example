bbfa46a1baa815fdf8631ef5c2af8a56cc2a5115
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-19 18:12:08 -0400

    chore(admin): login form values by default using envs

--- apps/admin/src/app/auth/login/page.tsx
+++ apps/admin/src/app/auth/login/page.tsx
@@ -2,6 +2,16 @@ import { AuthPage } from "@refinedev/antd";
 
 export default function LoginPage() {
   return (
-    <AuthPage type="login" forgotPasswordLink={false} registerLink={false} />
+    <AuthPage
+      type="login"
+      forgotPasswordLink={false}
+      registerLink={false}
+      formProps={{
+        initialValues: {
+          email: process.env.ECOMMERCE_ADMIN,
+          password: process.env.ECOMMERCE_PASSWORD,
+        },
+      }}
+    />
   );
 }

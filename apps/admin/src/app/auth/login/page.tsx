ffb823b84e8070ea3037f6b66c76ae5af58a6aaf
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-19 18:03:55 -0400

    fix(admin): login page forgot password and sign up links removal

--- apps/admin/src/app/auth/login/page.tsx
+++ apps/admin/src/app/auth/login/page.tsx
@@ -1,25 +1,7 @@
 import { AuthPage } from "@refinedev/antd";
-import { Link } from "@refinedev/core";
 
 export default function LoginPage() {
   return (
-    <AuthPage
-      type="login"
-      forgotPasswordLink={
-        <span className="text-xs">
-          <Link to="/auth/forgot-password">
-            <span className="text-blue-400 font-medium">Forgot Password?</span>
-          </Link>
-        </span>
-      }
-      registerLink={
-        <span className="text-xs">
-          Don’t have an account?{" "}
-          <Link to="/auth/register">
-            <span className="text-blue-400 font-medium">Sign up</span>
-          </Link>
-        </span>
-      }
-    />
+    <AuthPage type="login" forgotPasswordLink={false} registerLink={false} />
   );
 }

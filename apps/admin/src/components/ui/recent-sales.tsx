03116890fc59deb89b3650be53e221111052fc44
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-22 20:13:43 -0400

    fix(dashboard): vertically align avatar to start

--- apps/admin/src/components/ui/recent-sales.tsx
+++ apps/admin/src/components/ui/recent-sales.tsx
@@ -3,7 +3,7 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 export function RecentSales() {
   return (
     <div className="space-y-8">
-      <div className="flex items-center">
+      <div className="flex items-start flex-wrap justify-start">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/01.png" alt="Avatar" />
           <AvatarFallback>JL</AvatarFallback>
@@ -20,7 +20,7 @@ export function RecentSales() {
           </div>
         </div>
       </div>
-      <div className="flex items-center flex-wrap justify-start">
+      <div className="flex items-start flex-wrap justify-start">
         <Avatar className="flex h-9 w-9 items-center justify-center space-y-0">
           <AvatarImage src="/avatars/02.png" alt="Avatar" />
           <AvatarFallback>JL</AvatarFallback>
@@ -37,7 +37,7 @@ export function RecentSales() {
           </div>
         </div>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-start flex-wrap justify-start">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/03.png" alt="Avatar" />
           <AvatarFallback>JL</AvatarFallback>
@@ -54,7 +54,7 @@ export function RecentSales() {
           </div>
         </div>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-start flex-wrap justify-start">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/04.png" alt="Avatar" />
           <AvatarFallback>JL</AvatarFallback>
@@ -71,7 +71,7 @@ export function RecentSales() {
           </div>
         </div>
       </div>
-      <div className="flex items-center">
+      <div className="flex items-start flex-wrap justify-start">
         <Avatar className="h-9 w-9">
           <AvatarImage src="/avatars/05.png" alt="Avatar" />
           <AvatarFallback>JL</AvatarFallback>

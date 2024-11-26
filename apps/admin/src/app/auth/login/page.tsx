bf9178821fe118b761113783314091bb70dd36b1
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-26 12:43:32 -0400

    fix(admin): update user's email

--- apps/admin/src/app/auth/login/page.tsx
+++ apps/admin/src/app/auth/login/page.tsx
@@ -2,7 +2,7 @@ import { AuthPage } from "@refinedev/antd";
 import React from "react";
 
 export default function LoginPage() {
-  const email = process.env.ECOMMERCE_ADMIN ?? "admin@ecommerce.com";
+  const email = process.env.ECOMMERCE_ADMIN ?? "dihiw99962@merotx.com";
   const password = process.env.ECOMMERCE_PASSWORD ?? "A$RK-ERYBLBn2ir";
 
   return (

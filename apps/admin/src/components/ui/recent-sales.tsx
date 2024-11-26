7efa6b409e1e6006eea7390697398cd973ef6165
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-25 21:18:51 -0400

    fix(recent-sales): text size and flex vertical spacing

--- apps/admin/src/components/ui/recent-sales.tsx
+++ apps/admin/src/components/ui/recent-sales.tsx
@@ -25,15 +25,15 @@ export function RecentSales({ latestOrders }: Props) {
                   <AvatarFallback>{`${first_name.substring(0, 1)} ${last_name.substring(0, 1)}`}</AvatarFallback>
                 </Avatar>
                 <div className="ml-4 flex flex-1 flex-col sm:flex-row gap-y-1 sm:justify-start">
-                  <div className="flex flex-1 flex-col">
-                    <p className="text-sm font-medium leading-none mb-2">
+                  <div className="flex flex-1 flex-col gap-y-2">
+                    <p className="text-sm font-medium leading-none mb-0">
                       {`${first_name} ${last_name}`}
                     </p>
-                    <p className="text-sm text-muted-foreground mb-2">
+                    <p className="text-sm text-muted-foreground mb-0">
                       {email}
                     </p>
                   </div>
-                  <div className="font-medium flex flex-1 sm:justify-end sm:self-center">
+                  <div className="text-sm font-medium flex flex-1 sm:justify-end sm:self-center">
                     $
                     {new Intl.NumberFormat("en-US", {
                       minimumFractionDigits: 2,

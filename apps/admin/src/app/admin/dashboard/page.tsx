8b0cb89df8f6c066646c9414207f3c00e4c246a0
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-25 21:49:28 -0400

    chore(dashboard): refactor get data into hook

--- apps/admin/src/app/admin/dashboard/page.tsx
+++ apps/admin/src/app/admin/dashboard/page.tsx
@@ -11,124 +11,20 @@ import {
   ShoppingBagIcon,
   UserGroupIcon,
 } from "@heroicons/react/24/solid";
-import { baseSupabaseClient } from "@/app/providers/data/data-provider";
-import {
-  DashboardDataResponse,
-  DashboardOrder,
-  OrderSummary,
-} from "@/types/types";
 import { useCurrentUser } from "@/context/user-context";
+import { useDashboardData } from "@/hooks/use-dashboard-data";
 
 export default function DashboardPage() {
+  const {
+    dashboardMainMetrics,
+    ordersPerMonth,
+    latestOrders,
+    hasMonthlyRevenueData,
+    hasMonthlySubscriptionsData,
+    hasMonthlyOrdersData,
+  } = useDashboardData();
+
   const { usersData } = useCurrentUser();
-  const [dashboardMainMetrics, setDashboardMainMetrics] =
-    React.useState<DashboardDataResponse | null>(null);
-  const [ordersPerMonth, setOrdersPerMonth] = React.useState<
-    OrderSummary[] | null
-  >(null);
-  const [latestOrders, setLatestOrders] = React.useState<
-    DashboardOrder[] | null
-  >(null);
-
-  const getDashboardData = React.useCallback(
-    async (postgresFunctionName: string) => {
-      const response = await baseSupabaseClient.rpc(postgresFunctionName);
-
-      if (response) {
-        const { data, error } = response as {
-          data?: DashboardDataResponse | OrderSummary[] | DashboardOrder[];
-          error?: unknown;
-        };
-
-        if (error) {
-          console.error(error);
-          throw new Error((error as { message?: string }).message);
-        }
-
-        return data;
-      } else {
-        throw new Error("Failed to get dashboard data");
-      }
-    },
-    []
-  );
-  const getDashboardMainMetricsData = React.useCallback(async () => {
-    const data = (await getDashboardData(
-      "get_dashboard_data"
-    )) as DashboardDataResponse;
-
-    if (!data) {
-      return setDashboardMainMetrics(null);
-    }
-
-    setDashboardMainMetrics(data);
-  }, [getDashboardData]);
-  const getOrdersPerMonthData = React.useCallback(async () => {
-    const data = (await getDashboardData(
-      "get_orders_summary_per_month"
-    )) as OrderSummary[];
-
-    if (!data) {
-      return setOrdersPerMonth(null);
-    }
-
-    setOrdersPerMonth(data);
-  }, [getDashboardData]);
-  const getLatestOrdersData = React.useCallback(async () => {
-    const data = (await getDashboardData(
-      "get_latest_orders"
-    )) as DashboardOrder[];
-
-    if (!data) {
-      return setLatestOrders(null);
-    }
-
-    setLatestOrders(data);
-  }, [getDashboardData]);
-  React.useEffect(() => {
-    getDashboardMainMetricsData();
-    getOrdersPerMonthData();
-    getLatestOrdersData();
-  }, [getDashboardMainMetricsData, getLatestOrdersData, getOrdersPerMonthData]);
-
-  const hasMonthlyRevenueData = React.useMemo(
-    () =>
-      typeof dashboardMainMetrics?.result_orders_total === "number" &&
-      typeof dashboardMainMetrics?.orders_amount_difference_percentage ===
-        "number" &&
-      typeof dashboardMainMetrics?.is_orders_amount_percentage_positive ===
-        "boolean",
-    [
-      dashboardMainMetrics?.is_orders_amount_percentage_positive,
-      dashboardMainMetrics?.orders_amount_difference_percentage,
-      dashboardMainMetrics?.result_orders_total,
-    ]
-  );
-  const hasMonthlySubscriptionsData = React.useMemo(
-    () =>
-      typeof dashboardMainMetrics?.result_customers_count === "number" &&
-      typeof dashboardMainMetrics?.customers_difference_percentage ===
-        "number" &&
-      typeof dashboardMainMetrics?.is_customers_difference_percentage_positive ===
-        "boolean",
-    [
-      dashboardMainMetrics?.customers_difference_percentage,
-      dashboardMainMetrics?.is_customers_difference_percentage_positive,
-      dashboardMainMetrics?.result_customers_count,
-    ]
-  );
-  const hasMonthlyOrdersData = React.useMemo(
-    () =>
-      typeof dashboardMainMetrics?.result_orders_total === "number" &&
-      typeof dashboardMainMetrics?.orders_difference_percentage === "number" &&
-      typeof dashboardMainMetrics?.is_orders_percentage_positive === "boolean",
-    [
-      dashboardMainMetrics?.is_orders_percentage_positive,
-      dashboardMainMetrics?.orders_difference_percentage,
-      dashboardMainMetrics?.result_orders_total,
-    ]
-  );
-
   const welcomeMessage = React.useMemo(
     () =>
       `Welcome back${!!usersData && usersData?.firstName ? `, ${usersData?.firstName}` : ""}!`,

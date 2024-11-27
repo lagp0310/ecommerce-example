714823dab310bbbb556138658583046e0eb048cc
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-27 11:51:40 -0400

    fix: payment method reference to payment method type

--- supabase/migrations/20241126155954_payment_methods.sql
+++ supabase/migrations/20241126155954_payment_methods.sql
@@ -5,6 +5,8 @@ create table
     is_active boolean not null,
     created_at timestamp with time zone not null default (now() at time zone 'utc'::text),
     updated_at timestamp with time zone null,
+    payment_method_type uuid null,
     constraint payment_methods_pkey primary key (id),
-    constraint payment_methods_store_fkey foreign key (store) references stores (id) on update cascade on delete cascade
+    constraint payment_methods_store_fkey foreign key (store) references stores (id) on update cascade on delete cascade,
+    constraint payment_methods_payment_method_type_fkey foreign key (payment_method_type) references payment_method_types (id) on update cascade on delete cascade
   ) tablespace pg_default;

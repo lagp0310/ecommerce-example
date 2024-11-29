05ac3bd7293f382f8582a76bee8f84ab5e72e932
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-29 19:46:02 -0400

    feat(storefront): update types file

--- apps/storefront/src/types/types.ts
+++ apps/storefront/src/types/types.ts
@@ -2,3 +2,41 @@ export type FooterLink = {
   groupName?: string;
   links: { text: string; url: string }[];
 };
+
+export type StoreHighlight = {
+  icon: React.ReactNode;
+  title: string;
+  description: string;
+};
+
+export type Category = {
+  categoryId: string;
+  image?: React.ReactNode;
+  title: string;
+  url: string;
+};
+
+export type ProductTag = {
+  text: string;
+  type?: string;
+};
+
+export type Product = {
+  id: string;
+  name: string;
+  price: number;
+  discountedPrice?: number;
+  currencySymbol?: string;
+  currencyCode: string;
+  image?: React.ReactNode;
+  rating?: number;
+  tags?: ProductTag[];
+};
+
+export type CustomerTestimonial = {
+  id: string;
+  text: string;
+  avatarUrl: string;
+  rating: number;
+  fullName: string;
+};

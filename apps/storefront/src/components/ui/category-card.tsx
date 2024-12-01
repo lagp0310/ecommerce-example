9a64f278093b8fbe5da5ce49db97f65340c354a1
Luis Guerrero <luis.alberto199846@gmail.com>
2024-11-30 20:39:51 -0400

    fix(storefront): category card as ssr component with next link

--- apps/storefront/src/components/ui/category-card.tsx
+++ apps/storefront/src/components/ui/category-card.tsx
@@ -1,7 +1,27 @@
+import Link from "next/link";
 import React from "react";
+import isURL from "validator/es/lib/isURL";
 
-type Props = React.HTMLProps<HTMLDivElement>;
+type Props = React.HTMLProps<HTMLDivElement> & {
+  url?: string;
+  shouldUseNextLink?: boolean;
+};
 
-export function CategoryCard({ ...props }: Props) {
-  return <div {...props}></div>;
+export function CategoryCard({
+  children,
+  url,
+  shouldUseNextLink = true,
+  ...props
+}: Props) {
+  const isValidUrl = typeof url === "string" && isURL(url);
+
+  return (
+    <React.Fragment>
+      {isValidUrl && shouldUseNextLink ? (
+        <Link href={url}>{children}</Link>
+      ) : (
+        <div {...props}>{children}</div>
+      )}
+    </React.Fragment>
+  );
 }

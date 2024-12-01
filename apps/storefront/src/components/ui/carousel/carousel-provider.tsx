b627769dc22e90e7d8ebc373139018ce1c2e7e09
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-01 12:48:36 -0400

    fix(storefront): carousel provider should not render on desktop

--- apps/storefront/src/components/ui/carousel/carousel-provider.tsx
+++ apps/storefront/src/components/ui/carousel/carousel-provider.tsx
@@ -5,9 +5,16 @@ import {
   type CarouselProviderProps as ProviderProps,
   CarouselProvider as Provider,
 } from "pure-react-carousel";
+import { useIsMobile } from "@/hooks/useIsMobile";
 
 type Props = React.PropsWithChildren<ProviderProps>;
 
-export function CarouselProvider({ ...props }: Props) {
-  return <Provider {...props} />;
+export function CarouselProvider({ children, ...props }: Props) {
+  const isMobile = useIsMobile("(max-width: 640px)");
+
+  return (
+    <React.Fragment>
+      {isMobile ? <Provider {...props}>{children}</Provider> : children}
+    </React.Fragment>
+  );
 }

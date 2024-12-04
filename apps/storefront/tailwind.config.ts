988742bfdc78fcf178b97ce16a5414e22b9fecac
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-03 21:29:42 -0400

    feat(storefront): add shadcn navigation menu

--- apps/storefront/tailwind.config.ts
+++ apps/storefront/tailwind.config.ts
@@ -1,6 +1,7 @@
 import type { Config } from "tailwindcss";
 
 export default {
+  darkMode: ["class"],
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -17,28 +18,28 @@ export default {
         warning: "#FF8A00",
         danger: "#EA4B48",
         gray: {
-          50: "#F2F2F2",
-          100: "#E6E6E6",
-          200: "#CCCCCC",
-          300: "#B3B3B3",
-          400: "#999999",
-          500: "#808080",
-          600: "#666666",
-          700: "#4D4D4D",
-          800: "#333333",
-          900: "#1A1A1A",
+          "50": "#F2F2F2",
+          "100": "#E6E6E6",
+          "200": "#CCCCCC",
+          "300": "#B3B3B3",
+          "400": "#999999",
+          "500": "#808080",
+          "600": "#666666",
+          "700": "#4D4D4D",
+          "800": "#333333",
+          "900": "#1A1A1A",
         },
         "green-gray": {
-          50: "#EDF2EE",
-          100: "#DAE5DA",
-          200: "#B4CCB4",
-          300: "#96B297",
-          400: "#7A997C",
-          500: "#618062",
-          600: "#406B42",
-          700: "#2B572E",
-          800: "#173B1A",
-          900: "#002603",
+          "50": "#EDF2EE",
+          "100": "#DAE5DA",
+          "200": "#B4CCB4",
+          "300": "#96B297",
+          "400": "#7A997C",
+          "500": "#618062",
+          "600": "#406B42",
+          "700": "#2B572E",
+          "800": "#173B1A",
+          "900": "#002603",
         },
       },
       fontSize: {
@@ -57,14 +58,23 @@ export default {
       },
       keyframes: {
         "infinite-slide": {
-          "0%": { transform: "translateX(0)" },
-          "100%": { transform: "translateX(-1500px)" },
+          "0%": {
+            transform: "translateX(0)",
+          },
+          "100%": {
+            transform: "translateX(-1500px)",
+          },
         },
       },
       animation: {
         "infinite-carousel": "infinite-slide 20s linear infinite",
       },
+      borderRadius: {
+        lg: "var(--radius)",
+        md: "calc(var(--radius) - 2px)",
+        sm: "calc(var(--radius) - 4px)",
+      },
     },
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 } satisfies Config;

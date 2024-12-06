f02c3c9c453b650bb1e5730d6967411870af0b7e
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-06 11:39:58 -0400

    feat(storefront): custom logic for dots and uncomment on carousels

--- apps/storefront/src/components/ui/carousel/default-dot-group.tsx
+++ apps/storefront/src/components/ui/carousel/default-dot-group.tsx
@@ -11,22 +11,38 @@ export function DefaultDotGroup({ ...props }: Props) {
   return (
     <DotGroup
       {...props}
-      renderDots={({ currentSlide = 0, totalSlides = 0 }) => (
-        <React.Fragment>
-          {Array.from({ length: totalSlides }).map((_value, index) => (
-            <Dot
-              key={index}
-              slide={index}
-              className={cn(
-                "rounded-full h-2.5 w-2.5 bg-gray-100 first:ml-0 ml-2",
-                {
-                  "bg-primary": index === currentSlide,
-                }
-              )}
-            />
-          ))}
-        </React.Fragment>
-      )}
+      renderDots={({
+        currentSlide = 0,
+        totalSlides = 0,
+        visibleSlides = 1,
+      }) => {
+        const totalDots = Math.floor(totalSlides / visibleSlides);
+
+        return (
+          <React.Fragment>
+            {Array.from({ length: totalDots }).map((_value, index) => {
+              const slideIndex = index * visibleSlides;
+              const maxDotLimit = currentSlide + (visibleSlides - 1);
+              const isDotActive =
+                (currentSlide % 2 === 0 ? slideIndex : slideIndex + 1) >=
+                  currentSlide && slideIndex < maxDotLimit;
+
+              return (
+                <Dot
+                  key={index}
+                  slide={slideIndex}
+                  className={cn(
+                    "rounded-full h-2.5 w-2.5 bg-gray-100 first:ml-0 ml-2",
+                    {
+                      "bg-primary": isDotActive,
+                    }
+                  )}
+                />
+              );
+            })}
+          </React.Fragment>
+        );
+      }}
     />
   );
 }

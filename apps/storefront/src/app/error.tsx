4dbe30a45b3ca05ea67953f7124fd23e68ac798a
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-08 09:55:34 -0400

    chore(storefront): better error page

--- apps/storefront/src/app/error.tsx
+++ apps/storefront/src/app/error.tsx
@@ -14,9 +14,20 @@ export default function Error({
   }, [error]);
 
   return (
-    <div>
-      <h2>Something went wrong!</h2>
-      <button onClick={() => reset()}>Try again</button>
+    <div className="flex flex-1 flex-col justify-center items-center gap-y-4 py-20">
+      <h2 className="text-heading-5 text-center md:text-heading-3 font-semibold text-gray-900">
+        Oops! An error has ocurred
+      </h2>
+      <p className="text-center text-body-small md:text-body-medium font-normal text-gray-500">
+        Ut consequat ac tortor eu vehicula. Aenean accumsan purus eros. Maecenas
+        sagittis tortor at metus mollis
+      </p>
+      <button
+        className="text-body-small font-semibold text-white max-w-fit px-5 py-3 bg-primary rounded-full"
+        onClick={() => reset()}
+      >
+        Try again
+      </button>
     </div>
   );
 }

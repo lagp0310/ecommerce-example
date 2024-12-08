830ea5303c7e2887e9aff37c67575b8728fd42ed
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-08 09:52:58 -0400

    chore(storefront): better not found page

--- apps/storefront/src/app/not-found.tsx
+++ apps/storefront/src/app/not-found.tsx
@@ -1,11 +1,23 @@
+import { NotFoundIllustration } from "@/components/ui/illustrations/not-found";
 import Link from "next/link";
 
 export default function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
-      <p>Could not find requested resource</p>
-      <Link href="/">Return Home</Link>
+    <div className="flex flex-1 flex-col justify-center items-center gap-y-4 pb-20">
+      <NotFoundIllustration className="h-64 w-64 md:h-96 md:w-96" />
+      <h2 className="text-heading-5 text-center md:text-heading-3 font-semibold text-gray-900">
+        Oops! Page not found
+      </h2>
+      <p className="text-center text-body-small md:text-body-medium font-normal text-gray-500">
+        Ut consequat ac tortor eu vehicula. Aenean accumsan purus eros. Maecenas
+        sagittis tortor at metus mollis
+      </p>
+      <Link
+        href="/"
+        className="text-body-small font-semibold text-white max-w-fit px-5 py-3 bg-primary rounded-full"
+      >
+        Back to Home
+      </Link>
     </div>
   );
 }

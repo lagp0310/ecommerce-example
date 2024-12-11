9e1eaf62ce0fa3ecc95e68f36769900ef31b7519
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-11 18:40:36 -0400

    feat(storefront): add shadcn components for product listing page

--- apps/storefront/tailwind.config.ts
+++ apps/storefront/tailwind.config.ts
@@ -75,9 +75,27 @@ export default {
             transform: "translateX(-1500px)",
           },
         },
+        "accordion-down": {
+          from: {
+            height: "0",
+          },
+          to: {
+            height: "var(--radix-accordion-content-height)",
+          },
+        },
+        "accordion-up": {
+          from: {
+            height: "var(--radix-accordion-content-height)",
+          },
+          to: {
+            height: "0",
+          },
+        },
       },
       animation: {
         "infinite-carousel": "infinite-slide 20s linear infinite",
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       borderRadius: {
         lg: "var(--radius)",

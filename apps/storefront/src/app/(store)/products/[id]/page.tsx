07f777e0e47b6edd5e46c23a72894374314ad289
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-14 16:02:16 -0400

    chore(product): page wip

--- apps/storefront/src/app/(store)/products/[id]/page.tsx
+++ apps/storefront/src/app/(store)/products/[id]/page.tsx
@@ -1,3 +1,30 @@
+import { CarouselProvider } from "@/components/carousel/carousel-provider";
+import { CarouselRenderer } from "@/components/carousel/carousel-renderer";
+import { DefaultDotGroup } from "@/components/carousel/default-dot-group";
+import { DotsRenderer } from "@/components/carousel/dots-renderer";
+import { SlideRenderer } from "@/components/carousel/slide-renderer";
+import { Section } from "@/components/ui/common/section";
+import { SectionContent } from "@/components/ui/common/section-content";
+import { SectionTitle } from "@/components/ui/common/section-title";
+import { BasicProductCard } from "@/components/ui/product/basic-product-card";
+import { ProductPricing } from "@/components/ui/product/product-pricing";
+import { ProductTag } from "@/components/ui/product/product-tag";
+import { ProductTitle } from "@/components/ui/product/product-title";
+import { Rating } from "@/components/ui/product/rating";
+import {
+  defaultCarouselInterval,
+  defaultSlideHeight,
+  defaultSlideWidth,
+  product,
+} from "@/constants/constants";
+import { cn } from "@/lib/utils";
+import type {
+  CarouselProviderCustomProps,
+  CarouselRendererProps,
+} from "@/types/types";
+import { ArrowRightIcon, StarIcon } from "@heroicons/react/24/outline";
+import { StarIcon as FilledStarIcon } from "@heroicons/react/24/solid";
+import Link from "next/link";
 import React from "react";
 
 export default async function Product({
@@ -6,5 +33,161 @@ export default async function Product({
   params: Promise<{ id: string }>;
 }) {
   console.log(await params);
-  return <></>;
+
+  const {
+    name,
+    price,
+    currencySymbol,
+    discountedPrice,
+    image,
+    rating,
+    generalTags,
+    discountTags,
+    totalRatings,
+    description,
+  } = product;
+  const relatedProductsLength = 10;
+  const relatedProductsCarouselProviderProps: CarouselProviderCustomProps = {
+    naturalSlideHeight: defaultSlideHeight,
+    naturalSlideWidth: defaultSlideWidth,
+    totalSlides: relatedProductsLength,
+    interval: defaultCarouselInterval,
+    isPlaying: true,
+    infinite: true,
+    visibleSlides: 2,
+    renderInDesktop: true,
+    className:
+      "w-full h-60 min-[400px]:h-72 min-[500px]:h-80 min-[580px]:h-[350px] sm:h-[300px]",
+    mobileMediaQuery: "(max-width: 768px)",
+    visibleSlidesSm: { mediaQuery: "(max-width: 640px)", visibleSlides: 2 },
+    visibleSlidesMd: {
+      mediaQuery: "(min-width: 641px) and (max-width: 768px)",
+      visibleSlides: 3,
+    },
+    visibleSlidesLg: {
+      mediaQuery: "(min-width: 769px)",
+      visibleSlides: 5,
+    },
+  };
+  const relatedProductsCarouselRendererProps: CarouselRendererProps = {
+    mobileMediaQuery: "(max-width: 768px)",
+    renderInDesktop: true,
+    carouselSliderProps: {
+      className:
+        "h-60 min-[400px]:h-72 min-[500px]:h-80 min-[580px]:h-[350px] sm:h-[300px]",
+      classNameTray:
+        "h-60 min-[400px]:h-72 min-[500px]:h-80 min-[580px]:h-[350px] sm:h-[300px]",
+    },
+  };
+
+  return (
+    <div className="flex flex-1 flex-col items-center">
+      <div className="flex flex-1 flex-col px-6 py-8 xl:px-0 max-w-7xl">
+        <div className="flex flex-1 flex-row gap-x-4">
+          <div className="flex flex-1 flex-row justify-center items-center basis-1/2">
+            {image}
+          </div>
+          <div className="flex flex-col gap-6 basis-1/2">
+            <div className="flex flex-col gap-5 justify-center border-b border-gray-100">
+              <div className="flex flex-row gap-4 items-center">
+                <ProductTitle className="font-normal text-heading-4 text-gray-900">
+                  {name}
+                </ProductTitle>
+                <div className="flex flex-1 flex-row flex-wrap gap-2">
+                  {generalTags?.map((tag, index) => (
+                    <ProductTag key={index} tag={tag} />
+                  ))}
+                </div>
+              </div>
+              <div className="flex flex-row gap-2 items-center">
+                <Rating
+                  className="flex flex-row justify-end gap-x-0.5"
+                  rating={rating ?? 0}
+                  emptyIcon={<StarIcon className="size-[18px] text-warning" />}
+                  filledIcon={
+                    <FilledStarIcon className="size-[18px] text-warning" />
+                  }
+                />
+                <span className="font-normal text-body-small text-gray-600">{`${totalRatings} Reviews`}</span>
+              </div>
+              <div className="flex flex-row gap-4 items-center">
+                <ProductPricing
+                  className="flex flex-1 flex-row gap-2 items-center"
+                  currencySymbol={currencySymbol}
+                  price={price}
+                  // discountedPrice={discountedPrice}
+                  discountedPriceClasses={cn(
+                    "font-medium text-body-xxl text-hard-primary"
+                  )}
+                  priceClasses={cn(
+                    {
+                      "font-normal leading-[150%] text-body-xl text-gray-300":
+                        !!discountedPrice,
+                    },
+                    "font-medium text-body-xxl text-hard-primary"
+                  )}
+                  isFirstOnList
+                />
+                <div className="flex flex-1 flex-row flex-wrap gap-2 items-center">
+                  {discountTags?.map((tag, index) => (
+                    <ProductTag key={index} tag={tag} />
+                  ))}
+                </div>
+              </div>
+            </div>
+            <div className="flex flex-col gap-5 justify-center border-b border-gray-100">
+              <p className="font-normal text-body-small text-gray-500">
+                {description}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+      <Section className="flex flex-1 flex-col gap-y-8 px-6 xl:px-0">
+        <SectionTitle className="w-full max-w-7xl">
+          <div className="flex flex-1 flex-row">
+            <h2 className="text-body-xl font-semibold text-gray-900 md:text-heading-5">
+              Related Products
+            </h2>
+            <div className="flex flex-1 flex-row justify-end">
+              <Link
+                href="/products"
+                className="flex flex-row items-center justify-end gap-x-2 text-body-medium font-medium text-primary"
+              >
+                View All
+                <ArrowRightIcon className="size-4 text-primary" />
+              </Link>
+            </div>
+          </div>
+        </SectionTitle>
+        <SectionContent className="w-full max-w-7xl md:grid md:grid-cols-3 md:gap-6 lg:grid-cols-5">
+          <CarouselProvider {...relatedProductsCarouselProviderProps}>
+            <CarouselRenderer {...relatedProductsCarouselRendererProps}>
+              {Array.from({ length: relatedProductsLength }).map(
+                (_value, index) => (
+                  <SlideRenderer
+                    key={index}
+                    index={index}
+                    innerClassName="px-1 mx-auto"
+                    mobileMediaQuery="(max-width: 768px)"
+                  >
+                    <BasicProductCard
+                      product={product}
+                      isFirstOnList={index === 0}
+                    />
+                  </SlideRenderer>
+                )
+              )}
+            </CarouselRenderer>
+            <DotsRenderer mobileMediaQuery="(max-width: 768px)">
+              <DefaultDotGroup
+                disableActiveDots
+                className="mt-7 flex w-full flex-1 flex-row items-center justify-center gap-x-1"
+              />
+            </DotsRenderer>
+          </CarouselProvider>
+        </SectionContent>
+      </Section>
+    </div>
+  );
 }

3178d5029718ef7921fd468285238ca542dc6124
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-16 14:12:17 -0400

    chore(store-features): add render_order table column

--- supabase/migrations/20241126160033_store_features.sql
+++ supabase/migrations/20241126160033_store_features.sql
@@ -7,6 +7,7 @@ create table
     created_at timestamp with time zone not null default (now() at time zone 'utc'::text),
     updated_at timestamp with time zone null,
     store uuid not null,
+    render_order smallint null,
     constraint store_features_pkey primary key (id),
     constraint store_features_store_fkey foreign key (store) references stores (id) on update cascade on delete cascade
   ) tablespace pg_default;

f8942069bd8eb062458e84e399137262aca82a7b
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-18 16:15:57 -0400

    chore(product): product fragment in getProduct query

--- apps/storefront/src/gql/queries/product/queries.ts
+++ apps/storefront/src/gql/queries/product/queries.ts
@@ -23,36 +23,7 @@ export const getProduct = (params: string) => gql`
     productsCollection(${params}) {
       edges {
         node {
-          id
-          sku
-          slug
-          name
-          description
-          availableQuantity: available_quantity
-          price
-          discountedPrice: discounted_price
-          discountedUntil: discounted_until
-          currencies {
-            id
-            currencyCode: three_letter_code
-            currencySymbol: symbol
-          }
-          imageUrl: image_url
-          rating
-          product_tagsCollection {
-            edges {
-              node {
-                id
-                tag
-                isGeneralTag: is_general_tag
-                isDiscountTag: is_discount_tag
-                tagTypes: tag_types {
-                  type
-                }
-              }
-            }
-          }
-          createdAt: created_at
+          ...ProductFragment
         }
       }
       pageInfo {

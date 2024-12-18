92dbe78d0f9f57a35f7ad84273b1fc2ca7f18a74
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-18 09:00:22 -0400

    fix(products): rating filter must be a radio so it contextually tells the user that only one can be selected at a time

--- apps/storefront/src/components/ui/product/rating-filter-item-wrapper.tsx
+++ apps/storefront/src/components/ui/product/rating-filter-item-wrapper.tsx
@@ -1,12 +1,12 @@
 "use client";
 
 import React from "react";
-import { Checkbox } from "@/components/ui/common/checkbox";
 import { usePathname, useRouter, useSearchParams } from "next/navigation";
 import { Rating } from "@/components/ui/product/rating";
 import { StarIcon } from "@heroicons/react/24/outline";
 import { StarIcon as FilledStarIcon } from "@heroicons/react/24/solid";
 import { cn } from "@/lib/utils";
+import { RadioGroup, RadioGroupItem } from "@/components/ui/common/radio-group";
 
 type Props = {
   wrapperClassName?: string;
@@ -32,20 +32,25 @@ export function RatingFilterItemWrapper({
     }
 
     router.push(`${pathname}?${updatedSearchParams.toString()}`);
-  }, [pathname, router, searchParams]);
+  }, [checked, index, pathname, router, searchParams]);
 
   return (
-    <div
+    <RadioGroup
       className={cn(
         "group/rating flex flex-1 flex-row items-center gap-x-2",
         wrapperClassName
       )}
       onClick={handleRatingItemClick}
     >
-      <Checkbox
+      <RadioGroupItem
+        checked={checked}
+        value={(index + 1).toString()}
+        className="h-5 w-5 ring-primary outline-none focus-visible:ring-8 focus-visible:ring-offset-0 border border-gray-200 aria-checked:border-primary text-primary"
+      />
+      {/* <Checkbox
         checked={checked}
         className="size-5 rounded-[3px] border border-gray-100 bg-white text-gray-900 outline-none data-[state=checked]:border-none data-[state=checked]:bg-primary data-[state=checked]:text-white motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
-      />
+      /> */}
       <Rating
         className="flex flex-row justify-end gap-x-0.5"
         rating={index + 1}
@@ -56,6 +61,6 @@ export function RatingFilterItemWrapper({
         {(index + 1).toFixed(1)}
         <span className="group-first/rating:hidden"> and up</span>
       </span>
-    </div>
+    </RadioGroup>
   );
 }

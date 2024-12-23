b74b4e6453551822817ef9565a4cbe8327669076
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-23 16:49:40 -0400

    chore(storefront): lint fix

--- apps/storefront/src/app/(store)/products/[slug]/loading.tsx
+++ apps/storefront/src/app/(store)/products/[slug]/loading.tsx
@@ -6,8 +6,8 @@ export default function Loading() {
   return (
     <div className="my-6 flex flex-1 flex-col gap-y-6">
       <div className="flex flex-1 flex-row items-center justify-center px-6 xl:px-0">
-        <section className="flex w-full max-w-7xl flex-1 flex-col md:flex-row gap-6">
-          <Skeleton className="h-[350px] md:h-[550px] w-full md:basis-1/2 rounded-[10px]" />
+        <section className="flex w-full max-w-7xl flex-1 flex-col gap-6 md:flex-row">
+          <Skeleton className="h-[350px] w-full rounded-[10px] md:h-[550px] md:basis-1/2" />
           <div className="flex flex-1 basis-1/2 flex-col gap-y-6">
             {Array.from({ length: productSkeletonFields }).map(
               (_value, index) => (

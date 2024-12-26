7b27eb83441d719897efe44ac6f06dbf052a3fc0
Luis Guerrero <luis.alberto199846@gmail.com>
2024-12-25 20:19:42 -0400

    chore(storefront): fetchPolicy parameter for server query function

--- apps/storefront/src/lib/server-query.ts
+++ apps/storefront/src/lib/server-query.ts
@@ -1,18 +1,28 @@
 import type { Node } from "@/gql/graphql";
 import { getClient } from "@/lib/apollo-client";
 import type { BaseOperationVariables } from "@/types/types";
-import type { OperationVariables, DocumentNode } from "@apollo/client";
+import type {
+  OperationVariables,
+  DocumentNode,
+  FetchPolicy,
+} from "@apollo/client";
 
 const client = getClient();
 
 export async function queryGraphql<
   T,
   Q extends OperationVariables = BaseOperationVariables,
->(collectionName: string, query: DocumentNode, variables?: Q) {
+>(
+  collectionName: string,
+  query: DocumentNode,
+  variables?: Q,
+  fetchPolicy?: FetchPolicy
+) {
   try {
     const { data, error } = await client.query<T, Q>({
       query,
       variables,
+      fetchPolicy,
     });
 
     if (error) {

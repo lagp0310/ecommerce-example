67ffc9e48915fb729e13615372cc71b7f62fe203
Luis Guerrero <luis.alberto199846@gmail.com>
2025-01-01 19:49:20 -0400

    chore(checkout): wip cash form

--- apps/storefront/src/components/ui/checkout/cash-form.tsx
+++ apps/storefront/src/components/ui/checkout/cash-form.tsx
@@ -4,103 +4,96 @@ import { createOrder } from "@/app/(store)/checkout/actions";
 import { Form } from "@/components/form/form";
 import { Input } from "@/components/form/input";
 import { Label } from "@/components/form/label";
+import { useCart } from "@/context/cart-context";
 import React from "react";
+import {
+  type NumberFormatValues,
+  NumericFormat,
+  type NumericFormatProps,
+} from "react-number-format";
 
-export type Props = {};
+export type Props = { htmlNamePrefix: string };
 
-export function CashForm({ ...props }: Props) {
+export function CashForm({ htmlNamePrefix }: Props) {
   // TODO: Client and Server validation.
   // TODO: Error states.
   // TODO: Autofill data if the user is signed in.
+
+  const { cartSummary } = useCart();
+  const cartTotal = React.useMemo(
+    () => parseFloat(cartSummary.total.toFixed(2)),
+    [cartSummary.total]
+  );
+
+  const [change, setChange] = React.useState(0);
+  const handleAmountChange = React.useCallback(
+    (values: NumberFormatValues) => {
+      const floatValue = values.floatValue;
+
+      if (typeof floatValue !== "number") {
+        return;
+      }
+
+      setChange(floatValue - cartTotal);
+    },
+    [cartTotal]
+  );
+
+  const commonNumericFormatProps: NumericFormatProps = React.useMemo(
+    () => ({
+      customInput: Input,
+      allowLeadingZeros: true,
+      allowNegative: false,
+      decimalSeparator: ".",
+      fixedDecimalScale: true,
+      decimalScale: 2,
+      prefix: "$",
+      thousandSeparator: ",",
+    }),
+    []
+  );
 
   return (
     <Form action={createOrder} className="flex flex-1 flex-col gap-4">
       <div className="flex flex-1 flex-col gap-4">
         <div className="flex flex-1 flex-row flex-wrap md:flex-nowrap gap-4">
           <Label
-            htmlFor="first-name"
-            className="flex flex-col gap-y-2 w-full cursor-pointer"
+            htmlFor={`${htmlNamePrefix}-first-name`}
+            className="flex flex-col gap-y-2 cursor-pointer w-full md:w-fit"
           >
             <span className="font-normal text-body-small text-gray-900">
-              First Name<span className="text-danger"> *</span>
+              {`I'll pay...`}
+              <span className="text-danger"> *</span>
             </span>
-            <Input
-              id="first-name"
-              name="first-name"
-              placeholder="First Name"
+            <NumericFormat
+              onValueChange={handleAmountChange}
+              id={`${htmlNamePrefix}-first-name`}
+              name={`${htmlNamePrefix}-first-name`}
+              placeholder="Amount"
               type="text"
               required
               aria-required
-              className="focus:ring-2 focus:ring-primary/50 placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-small placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
+              className="w-full md:w-fit focus:ring-2 focus:ring-primary/50 placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-small placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
+              {...commonNumericFormatProps}
             />
           </Label>
           <Label
-            htmlFor="last-name"
-            className="flex flex-col gap-y-2 w-full cursor-pointer"
+            htmlFor={`${htmlNamePrefix}-change`}
+            className="flex flex-col gap-y-2 w-full md:w-fit"
           >
             <span className="font-normal text-body-small text-gray-900">
-              Last Name<span className="text-danger"> *</span>
+              {`Your change`}
             </span>
-            <Input
-              id="last-name"
-              name="last-name"
-              placeholder="Last Name"
+            <NumericFormat
+              value={change}
+              id={`${htmlNamePrefix}-change`}
+              name={`${htmlNamePrefix}-change`}
+              placeholder="Amount"
               type="text"
-              required
-              aria-required
-              className="focus:ring-2 focus:ring-primary/50 placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-small placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
-            />
-          </Label>
-          <Label
-            htmlFor="company-name"
-            className="flex flex-col gap-y-2 w-full cursor-pointer"
-          >
-            <span className="font-normal text-body-small text-gray-900">
-              {`Company Name (Optional)`}
-            </span>
-            <Input
-              id="company-name"
-              name="company-name"
-              placeholder="Company Name"
-              type="text"
-              aria-required={false}
-              className="focus:ring-2 focus:ring-primary/50 placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-small placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
-            />
-          </Label>
-        </div>
-        <div className="flex flex-1 flex-row flex-wrap md:flex-nowrap gap-4">
-          <Label
-            htmlFor="email"
-            className="flex flex-col gap-y-2 w-full cursor-pointer"
-          >
-            <span className="font-normal text-body-small text-gray-900">
-              Email<span className="text-danger"> *</span>
-            </span>
-            <Input
-              id="email"
-              name="email"
-              placeholder="Email Address"
-              type="email"
-              required
-              aria-required
-              className="focus:ring-2 focus:ring-primary/50 placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-small placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
-            />
-          </Label>
-          <Label
-            htmlFor="phone-number"
-            className="flex flex-col gap-y-2 w-full cursor-pointer"
-          >
-            <span className="font-normal text-body-small text-gray-900">
-              Phone Number<span className="text-danger"> *</span>
-            </span>
-            <Input
-              id="phone-number"
-              name="phone-number"
-              placeholder="Phone Number"
-              type="tel"
-              required
-              aria-required
-              className="focus:ring-2 focus:ring-primary/50 placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-small placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
+              disabled
+              aria-disabled
+              className="w-full md:w-fit focus:ring-2 focus:ring-primary/50 placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-small placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none cursor-default"
+              {...commonNumericFormatProps}
             />
           </Label>
         </div>

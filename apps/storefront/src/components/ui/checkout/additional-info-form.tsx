0bb6ff24acb3dbe13137bd410abff57e5a6f703d
Luis Guerrero <luis.alberto199846@gmail.com>
2025-01-02 07:57:29 -0400

    chore(checkout): additional info label and textarea style fixes

--- apps/storefront/src/components/ui/checkout/additional-info-form.tsx
+++ apps/storefront/src/components/ui/checkout/additional-info-form.tsx
@@ -14,7 +14,10 @@ export function AdditionalInfoForm({ ...props }: Props) {
     <Form className="flex flex-1 flex-col gap-4">
       <div className="flex flex-1 flex-col gap-4">
         <div className="flex flex-1 flex-row gap-4">
-          <Label htmlFor="order-notes" className="flex flex-col gap-y-2 w-full">
+          <Label
+            htmlFor="order-notes"
+            className="flex flex-col gap-y-2 w-full cursor-pointer"
+          >
             <span className="font-normal text-body-small text-gray-900">
               {`Order Notes (Optional)`}
             </span>
@@ -23,7 +26,7 @@ export function AdditionalInfoForm({ ...props }: Props) {
               name="order-notes"
               placeholder="Notes about your order, e.g. special notes for delivery."
               aria-required={false}
-              className="focus:ring-2 focus:ring-primary/50 resize-y min-h-[100px] max-h-[500px] placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-medium placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3"
+              className="focus:ring-2 focus:ring-primary/50 resize-y min-h-[100px] max-h-[500px] placeholder:text-gray-400 placeholder:font-normal placeholder:text-body-medium placeholder:leading-[130%] rounded-six border border-gray-100 outline-none p-3 motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none"
             />
           </Label>
         </div>

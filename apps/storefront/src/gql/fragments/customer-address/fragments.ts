c04bc727b55551f0fc88e363fad17878195e3b7b
Luis Guerrero <luis.alberto199846@gmail.com>
2025-01-04 11:14:32 -0400

    feat(customer-address): fragments and mutations

---
+++ apps/storefront/src/gql/fragments/customer-address/fragments.ts
@@ -0,0 +1,32 @@
+import { gql } from "@apollo/client";
+
+export const customerAddressFragment = gql`
+  fragment CustomerAddressFragment on customer_addresses {
+    id
+    streetAddress: street_address
+    firstName: first_name
+    lastName: last_name
+    companyName: company_name
+    countryId: country
+    countryStateId: country_state
+    zipCode: zip_code
+    email
+    phoneNumber: phone_number
+    country: countries {
+      id
+      name
+      alpha2: alpha_2
+    }
+    countryState: country_states {
+      id
+      name
+      shortName: short_name
+    }
+    customerAddressType: customer_address_types {
+      id
+      type
+    }
+    createdAt: created_at
+    updatedAt: updated_at
+  }
+`;

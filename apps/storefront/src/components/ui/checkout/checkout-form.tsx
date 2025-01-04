54555e123e34051f510408e9556f7a70ae0c4455
Luis Guerrero <luis.alberto199846@gmail.com>
2025-01-04 14:24:55 -0400

    feat(checkout): billing and shipping address id form props

--- apps/storefront/src/components/ui/checkout/checkout-form.tsx
+++ apps/storefront/src/components/ui/checkout/checkout-form.tsx
@@ -6,16 +6,22 @@ import {
   Checkbox,
   type Props as CheckboxProps,
 } from "@/components/ui/common/checkbox";
-import type { GetParsedOptionsResponse } from "@/types/types";
+import type {
+  AddressTypesResponse,
+  GetParsedOptionsResponse,
+} from "@/types/types";
 import { AddressForm } from "@/components/ui/checkout/address-form";
 import { cn } from "@/lib/utils";
+import { AddressType } from "@/types/form/types";
 
 export type Props = {
+  customerId: string;
+  customerAddressTypes: AddressTypesResponse[];
   countries: GetParsedOptionsResponse;
   countryStates: GetParsedOptionsResponse;
 };
 
-export function CheckoutForm({ ...formProps }: Props) {
+export function CheckoutForm({ customerAddressTypes, ...formProps }: Props) {
   // TODO: Client and Server validation.
   // TODO: Error states.
   // TODO: Autofill data if the user is signed in.
@@ -36,9 +42,30 @@ export function CheckoutForm({ ...formProps }: Props) {
     [showShippingAddressForm, setShowShippingAddressForm]
   );
 
+  const billingAddressTypeId = React.useMemo(
+    () =>
+      customerAddressTypes.find(
+        ({ type }) => type.toLowerCase() === AddressType.BILLING.toLowerCase()
+      )?.id,
+    [customerAddressTypes]
+  );
+  const shippingAddressTypeId = React.useMemo(
+    () =>
+      customerAddressTypes.find(
+        ({ type }) => type.toLowerCase() === AddressType.SHIPPING.toLowerCase()
+      )?.id,
+    [customerAddressTypes]
+  );
+
   return (
     <div className="flex flex-1 flex-col gap-4">
-      <AddressForm {...formProps} htmlNamePrefix="billing" />
+      {!!billingAddressTypeId ? (
+        <AddressForm
+          {...formProps}
+          htmlNamePrefix="billing"
+          addressTypeId={billingAddressTypeId}
+        />
+      ) : null}
       <Label
         htmlFor="ship-different-address"
         className="flex flex-row gap-2 w-fit items-center"
@@ -54,7 +81,13 @@ export function CheckoutForm({ ...formProps }: Props) {
           { hidden: !showShippingAddressForm }
         )}
       >
-        <AddressForm {...formProps} htmlNamePrefix="shipping" />
+        {!!shippingAddressTypeId ? (
+          <AddressForm
+            {...formProps}
+            htmlNamePrefix="shipping"
+            addressTypeId={shippingAddressTypeId}
+          />
+        ) : null}
       </div>
     </div>
   );

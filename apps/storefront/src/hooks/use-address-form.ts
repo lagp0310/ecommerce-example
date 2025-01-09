608a984979d618cc86a7a727d06669e641a08050
Luis Guerrero <luis.alberto199846@gmail.com>
2025-01-08 20:51:21 -0400

    chore(address): useAddressForm hook now uses the customer-context value

--- apps/storefront/src/hooks/use-address-form.ts
+++ apps/storefront/src/hooks/use-address-form.ts
@@ -1,37 +1,50 @@
 "use client";
 
 import { saveAddressInformationAction } from "@/actions/customer-address/actions";
+import { useCart } from "@/context/cart-context";
+import { useCustomer } from "@/context/customer-context";
 import { addressFormSchema, type AddressForm } from "@/types/form/types";
 import { zodResolver } from "@hookform/resolvers/zod";
 import React from "react";
 import { type SubmitHandler, useForm } from "react-hook-form";
 
-export function useAddressForm(currentValues?: AddressForm) {
+export function useAddressForm(
+  addressTypeId: string,
+  currentValues?: AddressForm
+) {
+  const { cart } = useCart();
+  const { customer } = useCustomer();
+  const customerId = React.useMemo(() => customer?.id ?? "", [customer?.id]);
+
   const [isPending, startTransition] = React.useTransition();
 
   const { reset, ...form } = useForm<AddressForm>({
     resolver: zodResolver(addressFormSchema),
-    defaultValues: currentValues,
+    defaultValues: { ...currentValues, customerId },
   });
 
   React.useEffect(() => {
     if (currentValues) {
-      reset({ ...currentValues });
+      reset({ ...currentValues, customerId });
     }
-  }, [currentValues, reset]);
+  }, [currentValues, customerId, reset]);
 
   const onSubmit: SubmitHandler<AddressForm> = React.useCallback(
     async (data) => {
       try {
         startTransition(async () => {
-          const response = await saveAddressInformationAction(data);
+          const response = await saveAddressInformationAction({
+            ...data,
+            cart: cart?.id ?? "",
+            addressType: addressTypeId,
+          });
           console.log(response);
         });
       } catch (error) {
         throw new Error("Submit error on Address Form");
       }
     },
-    []
+    [addressTypeId, cart?.id]
   );
 
   const providerValue = React.useMemo(

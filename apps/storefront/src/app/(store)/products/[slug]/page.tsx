77c51a5056a573f411982d15f3aebcde3724e8ba
Luis Guerrero <luis.alberto199846@gmail.com>
2025-01-12 20:57:57 -0400

    fix(storefront): product page add to cart button height fix

--- apps/storefront/src/app/(store)/products/[slug]/page.tsx
+++ apps/storefront/src/app/(store)/products/[slug]/page.tsx
@@ -98,10 +98,10 @@ export default async function Product({
   const relatedProducts = parseProductTags(relatedProductsResult);
 
   const addToCartWrapperProps: Partial<AddToCartWrapperProps> = {
-    wrapperClassName: "flex flex-row w-fit h-[30px] md:h-[45px] mt-3 md:mt-0",
+    wrapperClassName: "flex flex-row w-fit h-[45px] mt-3 md:mt-0",
     counterWrapperClassName: "flex",
     className:
-      "group flex h-[30px] md:h-[45px] flex-1 flex-row items-center justify-center gap-x-2 rounded-full bg-primary text-body-small font-semibold leading-6 text-white hover:border hover:border-primary hover:bg-white hover:text-primary disabled:cursor-not-allowed disabled:border-none disabled:opacity-50 disabled:transition-none disabled:hover:bg-primary disabled:hover:text-white motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none",
+      "group flex h-[45px] flex-1 flex-row items-center justify-center gap-x-2 rounded-full bg-primary text-body-small font-semibold leading-6 text-white hover:border hover:border-primary hover:bg-white hover:text-primary disabled:cursor-not-allowed disabled:border-none disabled:opacity-50 disabled:transition-none disabled:hover:bg-primary disabled:hover:text-white motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none",
     minusClassName:
       "disabled:cursor-not-allowed disabled:opacity-50 disabled:hover:bg-transparent disabled:hover:border-gray-100 rounded-full border border-gray-100 p-4 flex flex-1 flex-row items-center justify-center group/minus-button hover:border-transparent hover:bg-primary motion-safe:transition motion-safe:duration-100 motion-safe:ease-linear motion-reduce:transition-none",
     moreClassName:
@@ -183,7 +183,7 @@ export default async function Product({
                 {description}
               </p>
             </div>
-            <div className="flex h-[30px] flex-row items-center gap-3 pb-5 md:h-[45px]">
+            <div className="flex h-[45px] flex-row items-center gap-3 pb-5">
               {!!product ? (
                 <AddToCartWrapper {...addToCartWrapperProps} product={product}>
                   Add to Cart
